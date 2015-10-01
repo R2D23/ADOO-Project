18aba163be12615808c100eeb3fb50f8b1109d1a
R2D23 <R2D23@hotmail.es>
2015-10-01 00:56:41 -0500

    Update

---
+++ src/adooui/GUI.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adooui;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author LuisArturo
+ */
+public class GUI extends JFrame implements Runnable, ActionListener{
+    private JPanel canvas;
+    private JPanel toolkit;
+    private Members members;
+    
+    public GUI(){
+	canvas = new JPanel();
+	toolkit = new JPanel();
+	members = new Members(this);
+    }
+    
+    @Override
+    public void run() {
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setMinimumSize(new Dimension(800, 600));
+	this.getContentPane().setLayout(null);
+	
+	members.setActionDesc("collaborators");
+	
+	canvas.setBackground(Color.WHITE);
+	canvas.setLocation(50, 50);
+	canvas.setSize(this.getWidth() - 100, this.getHeight() - 100);
+	this.getContentPane().add(members);
+	this.getContentPane().add(canvas);
+	
+	this.pack();
+	
+	showGUI();
+    }
+    
+    private void showGUI(){
+	this.setVisible(true);
+    }
+    
+    @Override
+    public void update(Graphics g){
+	super.update(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+	if(ae.getActionCommand().equals("collaborators"))
+	    System.out.println("Collaborator Window");
+    }
+}
