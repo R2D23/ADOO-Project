1cf97bc065fe0ce42186ca67268621220377a169
Angeles <Angeles@Cerritos>
2015-11-08 21:56:47 -0600

    Cambio de algunos metodos

--- src/core/Text.java
+++ src/core/Text.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.io.Serializable;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,7 +21,6 @@ public class Text extends Element implements Serializable{
     
     public Text(String m, Color c, int t)
     {
-        //posX = posY = 0;
         mensaje = m;
         colorLetra = c;
         tamanio = t;
@@ -31,57 +31,37 @@ public class Text extends Element implements Serializable{
     {
         mensaje = m;
         tamanio = 50;
-        
-        posX=posY = 200;
-        area = getArea();
         incline = 0;
+        posX=posY = 200;
+        getArea();
     }
     
+    @Override
     public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
-        //g2.setColor(color);
         g2.rotate(incline, posX +  mensaje.length()*tamanio/2, posY+tamanio/2);
 	g2.setFont(new Font("Serif", Font.ITALIC | Font.BOLD, tamanio));
 	g2.drawChars(mensaje.toCharArray(), 0, mensaje.length(),posX, posY);
-        //g2.drawString(mensaje, posX, posX);
-        //g2.fillRect(posX, posY, (int)width, (int)height);
-        //g2.setColor(lncolor);
-        //g2.drawRect(posX, posY, (int)width, (int)height);
-        /*if(state!=AVAILABLE) {
-            g2.setColor(Util.negative(bgcolor));
-        } else {
-            g2.setColor(bgcolor);
-        }*/
-        //area = new Area(new java.awt.Rectangle(posX, posY, (int)width, (int)height));
-        //g2.fill(area);
-        /*if(state!=AVAILABLE) {
-            g2.setColor(Util.negative(lncolor));
-        } else {
-            g2.setColor(lncolor);
-        }*/
         //g2.draw(area);
     }
 
+    @Override
     public void configure(Canvas canvas) {
-        area = new Area(new java.awt.Rectangle(posX+ mensaje.length()*tamanio, posY + tamanio, mensaje.length()*tamanio, tamanio));
-        AffineTransform rot = new AffineTransform();
-        rot.setToRotation(incline, posX +  mensaje.length()*tamanio/2, posY+tamanio/2);
-        area.transform(rot);
-        
+        JOptionPane.showMessageDialog(null, "No disponible aun");
     }
-    public Area getArea()
+    @Override
+    public void getArea()
     {
         area = new Area(new java.awt.Rectangle(posX, posY - tamanio, mensaje.length()*tamanio, tamanio));
         AffineTransform rot = new AffineTransform();
         rot.setToRotation(incline, posX +  mensaje.length()*tamanio/2, posY+tamanio/2);
         area.transform(rot);
-        return area;
     }
     
     public void doZoom(float escala)
     {
         this.tamanio *= (1+escala);  
-        area = getArea();
+        getArea();
     }
     
     public void rotate(java.awt.Point e) {
@@ -91,14 +71,14 @@ public class Text extends Element implements Serializable{
         this.incline = Math.atan(pendiente) + Math.PI/2;
         if(X < 0)
             this.incline += Math.PI;
-        area = getArea();
+        getArea();
     }
     
     public void move(int x, int y)
     {
         posX = (int)(x - mensaje.length()*tamanio/2);
         posY = (int)(y - tamanio/2);
-        area = getArea();
+        getArea();
     }
     
     
