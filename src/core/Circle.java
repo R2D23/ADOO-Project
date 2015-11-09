b1066d062806dd17d23b74d8965b66de157901e2
Angeles <Angeles@Cerritos>
2015-11-08 21:59:08 -0600

    se cambiaron los metodos move, rotate y configure

--- src/core/Circle.java
+++ src/core/Circle.java
@@ -1,12 +1,10 @@
 
 package core;
 
-import core.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
@@ -25,52 +23,47 @@ public class Circle extends Figure{
     
     public Circle() {}
     
-   
+
+    @Override
     public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
-        try{
-            if(state!=AVAILABLE) {
-                g2.setColor(Util.negative(bgcolor));
-            } else {
-                g2.setColor(bgcolor);
-            }
+        if(state!=AVAILABLE) {
+            g2.setColor(Util.negative(bgcolor));
             g2.fill(area);
-            if(state!=AVAILABLE) {
-                g2.setColor(Util.negative(lncolor));
-            } else {
-                g2.setColor(lncolor);
-            }
+            g2.setColor(Util.negative(lncolor));
+            g2.draw(area);
+        } else {
+            g2.setColor(bgcolor);
+            g2.fill(area);
+            g2.setColor(lncolor);
             g2.draw(area);
         }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+        
     }
 
+    @Override
     public void configure(Canvas canvas) {
-        area = new Area(new Ellipse2D.Double(posX, posY, (int)radio,(int)radio));
-        AffineTransform rot = new AffineTransform();
-        rot.setToRotation(incline, posX+radio/2, posY+radio/2);
-        area.transform(rot);
+        new ConfigurarCirculo(canvas,this,new Point(this.posX, this.posY)).setVisible(true);
     }
     
-    public Area getArea()
+    @Override
+    public void getArea()
     {
         area = new Area(new Ellipse2D.Double(posX, posY, (int)radio,(int)radio));
         AffineTransform rot = new AffineTransform();
         rot.setToRotation(incline, posX+radio/2, posY+radio/2);
         area.transform(rot);
-        return area;
     }
     
+    @Override
     public void doZoom(float escala)
     {   
         super.doZoom(escala);
         this.radio *= (1+escala);
-        area = getArea();
+        getArea();
     }
     
+    @Override
      public void rotate(Point e) {
         double Y = (posY + radio/2) - e.getY();
         double X = (posX + radio/2) - e.getX();
@@ -78,14 +71,14 @@ public class Circle extends Figure{
         this.incline = Math.atan(pendiente) + Math.PI/2;
         if(X < 0)
             this.incline += Math.PI;
-        area = getArea();
-        
+        getArea();
     }
      
+    @Override
     public void move(int x, int y)
     {
         posX = (int)(x - radio/2);
         posY = (int)(y - radio/2);
-        area = getArea();
+        getArea();
     }
 }
